a43be969769bc3d3448bd283dcd5c175e07172f0
wkj <2672126643@qq.com>
2022-02-04 15:10:30 +0800

    忽略

--- demo/hello/hello.py
+++ demo/hello/hello.py
@@ -3,3 +3,27 @@ app=Flask(__name__)
 @app.route('/')
 def index():
 	return '<h1>Hello World</h1>'
+
+
+
+from appium import webdriver
+
+# 定义运行环境
+
+desired_caps = {
+	'devicename':'Android Emulator',
+	'automationName':'appium',
+	'platformName':'Android',
+	'platformVersion':'7.0',
+	'appPackage':'com.android.calculator2',
+	'appActivity':'.Calculator',
+
+}
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+driver.find_element_by_id("com.android.calculator2:id/digit_1").click()
+driver.find_element_by_id("com.android.calculator2:id/op_add").click()
+driver.find_element_by_id("com.android.calculator2:id/digit_2").click()
+driver.find_element_by_id("com.android.calculator2:id/eq").click()
+
+driver.quit()
